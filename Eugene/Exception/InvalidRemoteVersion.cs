5c34fade66633a85b0cf7bc85be29a48108d0545
JValck <jasper_de_valck@hotmail.com>
2016-01-10 19:34:35 +0100

    Update library type

---
+++ Eugene/Exception/InvalidRemoteVersion.cs
@@ -0,0 +1,17 @@
+﻿namespace Eugene.Exception
+{
+    class InvalidRemoteVersion:EugeneException
+    {
+        public InvalidRemoteVersion()
+        {
+        }
+
+        public InvalidRemoteVersion(string message) : base(message)
+        {
+        }
+
+        public InvalidRemoteVersion(string message, System.Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
